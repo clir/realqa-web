42a338f74d2d758c00b4745ca555f8f94e762454
spzheng <stephanie.p.zheng@gmail.com>
2014-11-30 15:53:24 -0500

    logout button and help page

--- src/main/java/edu/emory/clir/realqa/web/realqawebapp/realqa/urls.py
+++ src/main/java/edu/emory/clir/realqa/web/realqawebapp/realqa/urls.py
@@ -5,8 +5,9 @@ from realqa import views
 
 urlpatterns = patterns('', 
     url(r'^login$', TemplateView.as_view(template_name='realqa/login.html'), name='login'),
-	url(r'^logout$', views.logout, name='logout'),
+	url(r'^logout$', TemplateView.as_view(template_name='realqa/logout.html'), name='logout'),
     url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^help$', TemplateView.as_view(template_name='realqa/help.html'), name='help'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
 	url(r'^register$', TemplateView.as_view(template_name='realqa/register.html'), name='register'),
 	url(r'^inbox$', TemplateView.as_view(template_name='realqa/inbox.html'), name='inbox'),
